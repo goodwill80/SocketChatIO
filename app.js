af0345aa48e2c179609cb2b62d5d3536ce14e597
kit jonathan ho <j.hokit80@gmail.com>
2016-09-27 12:17:39 +0800

    node modules

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 // MY MAIN APP JS
 
-var express = require('./config/express');
-var mongoose = require('./config/mongoose');
-
-// run database before express application object
-var db = mongoose();
+var express = require('express');
 var app = express();
+var ejs = require('ejs');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
-app.set('port', (process.env.PORT || 7000));
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(express.static('./public'));
 
-app.listen(app.get('port'), function() {
-  console.log('My express server is running at localhost', app.get('port'));
-});
 
-//We are only exporting in case we wish to conduct testing, otherwise we do not need to export app
-module.exports = app;
+
+app.get('/', function(req, res){
+  res.render('index.html');
+})
+
+
+io.sockets.on('connection', function(socket){
+  //send message event
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data} );
+  })
+})
+
+
+
+
+
+
+server.listen(process.env.PORT || 9000);
+
+// app.listen(app.get('port'), function() {
+//   console.log('My express server is running at localhost', app.get('port'));
+// });
+//
+// //We are only exporting in case we wish to conduct testing, otherwise we do not need to export app
+// module.exports = app;
